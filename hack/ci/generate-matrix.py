21252f0ca632dbbaf16b7caab87f975bdc3229be
Alex Zenla <alex@edera.dev>
2024-05-09 02:15:41 -0700

    speed up matrix builds and use only lts releases and stable

--- hack/ci/generate-matrix.py
+++ hack/ci/generate-matrix.py
@@ -1,59 +1,42 @@
 import sys
 import json
-from packaging.version import Version
+from packaging.version import Version, parse
 
 data_path = sys.argv[1]
 matrix_path = sys.argv[2]
 
-all_versions = open('%s/all-versions' % data_path, 'r').read().strip().splitlines()
 release_info = json.loads(open('%s/releases.json' % data_path, 'r').read())
 latest_stable = release_info['latest_stable']['version']
 
-def version_parts(version: str) -> (int, int, int):
-  parts = version.split('.')
-  if len(parts) == 1:
-    return (int(parts[0]), 0, 0)
-  elif len(parts) == 2:
-    return (int(parts[0]), int(parts[1]), 0)
-  elif len(parts) == 3:
-    return (int(parts[0]), int(parts[1]), int(parts[2]))
-  else:
-    raise Exception("invalid version %s" % version
-)
+known_releases = []
 
-def version_greater(left: (int, int, int), right: (int, int, int)) -> bool:
-  if left[0] > right[0]:
-    return True
-  if left[1] > right[1]:
-    return True
-  if left[2] > right[2]:
-    return True
-  return False
+for release in release_info['releases']:
+  if release['moniker'] in ['stable', 'longterm']:
+    known_releases.append(release['version'])
 
 tags = {}
 major_minors = {}
 
-for version in all_versions:
-  parts = version_parts(version)
-
-  if parts[0] < 5:
+for version in known_releases:
+  parts = parse(version)
+  if parts.major < 5:
     continue
 
   if version == latest_stable:
     tags['stable'] = version
-  major = str(parts[0])
-  major_minor = "%s.%s" % (parts[0], parts[1])
+  major = str(parts.major)
+  major_minor = "%s.%s" % (parts.major, parts.minor)
 
   if major in tags:
     existing = tags[major]
-    if version_greater(parts, version_parts(existing)):
+    if parse(existing) < parts:
       tags[major] = version
   else:
     tags[major] = version
 
   if major_minor in tags:
     existing = tags[major_minor]
-    if version_greater(parts, version_parts(existing)):
+    if parse(existing) < parts:
       tags[major_minor] = version
       major_minors[major_minor] = version
   else:
@@ -77,8 +60,8 @@ for version in unique_versions:
     tag_version = tags[tag]
     if tag_version == version:
       version_tags.append(tag)
-  parts = version_parts(version)
-  src_url = "https://cdn.kernel.org/pub/linux/kernel/v%s.x/linux-%s.tar.xz" % (parts[0], version)
+  parts = parse(version)
+  src_url = "https://cdn.kernel.org/pub/linux/kernel/v%s.x/linux-%s.tar.xz" % (parts.major, version)
   version_builds.append({
     "version": version,
     "tags": version_tags,
--- hack/ci/generate-matrix.sh
+++ hack/ci/generate-matrix.sh
@@ -6,23 +6,6 @@ cd "$(dirname "${0}")/../.."
 mkdir -p target
 DATA_TMP_DIR="$(mktemp -d)"
 trap "rm -rf ${DATA_TMP_DIR}" EXIT
-
-major_versions() {
-  rsync --list-only --out-format='%n' "rsync://rsync.kernel.org/pub/linux/kernel/" | \
-    awk '{ print $5 }' | grep -E '^v(.*)x$' | awk -F '.x' '{print $1}' | sed 's/^v//'
-}
-
-release_versions() {
-  rsync --list-only --out-format='%n' "rsync://rsync.kernel.org/pub/linux/kernel/v${1}.x/" | \
-    awk '{print $5}' | grep '.xz$' | grep "linux-" | awk -F '-' '{print $2}' | \
-    awk -F '.tar' '{print $1}' | sort --version-sort
-}
-
-major_versions | while read -r MAJOR_VERSION
-do
-  release_versions "${MAJOR_VERSION}"
-done | sort --version-sort > "${DATA_TMP_DIR}/all-versions"
-
 KERNEL_RELEASES_JSON="${DATA_TMP_DIR}/releases.json"
 curl -sfL https://www.kernel.org/releases.json > "${KERNEL_RELEASES_JSON}"
 rm -rf "target/matrix.json"
