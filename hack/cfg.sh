24f9ce73e380c705b406e2dc191400ea18a88771
Alex Zenla <alex@edera.dev>
2024-05-13 04:23:49 +0000

    disable all modules in aarch64

--- hack/cfg.sh
+++ hack/cfg.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+CMD="${1}"
+if [ -z "${CMD}" ]
+then
+  CMD="nconfig"
+fi
+
 REAL_SCRIPT="$(realpath "${0}")"
 cd "$(dirname "${REAL_SCRIPT}")/.."
 KERNEL_DIR="$(realpath "${PWD}")"
@@ -12,5 +18,5 @@ rm -rf "${MODULES_INSTALL_PATH}"
 rm -rf "${ADDONS_OUTPUT_PATH}"
 rm -rf "${ADDONS_SQUASHFS_PATH}"
 
-make -C "${KERNEL_SRC}" ARCH="${TARGET_ARCH_KERNEL}" "${CROSS_COMPILE_MAKE}" INSTALL_MOD_PATH="${MODULES_INSTALL_PATH}" nconfig
+make -C "${KERNEL_SRC}" ARCH="${TARGET_ARCH_KERNEL}" "${CROSS_COMPILE_MAKE}" INSTALL_MOD_PATH="${MODULES_INSTALL_PATH}" "${CMD}"
 cp "${KERNEL_SRC}/.config" "${KERNEL_CONFIG_FILE}"
