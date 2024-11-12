cf263c824b46fc1476072f44fee94d01985b815e
Alex Zenla <alex@edera.dev>
2024-11-12 09:08:52 -0500

    add image production conflict check

--- hack/build/generate-matrix.py
+++ hack/build/generate-matrix.py
@@ -7,6 +7,7 @@ backbuild_matrix = matrix.generate_backbuild_matrix()
 all_matrix = matrix.merge_matrix([stable_matrix, backbuild_matrix])
 only_new_matrix = matrix.filter_new_builds(all_matrix)
 final_matrix = matrix.generate_final_matrix(only_new_matrix)
+matrix.validate_produce_conflicts(final_matrix)
 
 with open("matrix.json", "w") as f:
     json.dump(final_matrix, f)
