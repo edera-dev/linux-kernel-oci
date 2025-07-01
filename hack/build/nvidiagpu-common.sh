7f55635542a870e2869d6675972da4839e2c62b9
Benjamin Leggett <benjamin@edera.io>
2025-07-01 14:53:01 -0400

    fixup

--- hack/build/nvidiagpu-common.sh
+++ hack/build/nvidiagpu-common.sh
@@ -12,16 +12,12 @@ echo "Cloning nvidia out-of-tree module repo"
 
 git clone "${NV_KMOD_REPO} -b ${NV_KMOD_REPO_BRANCH}" ${NV_CLONE_PATH}
 
+OLDPWD=$(pwd)
 cd $NV_CLONE_PATH
 
 # make SYSSRC=~/Source/edera-dev/linux-kernel-oci/linux-6.14.6 SYSOUT=/home/bleggett/Source/edera-dev/linux-kernel-oci/obj INSTALL_MOD_PATH=/home/bleggett/Source/edera-dev/linux-kernel-oci/obj modules_instal
-#
-# make -C "${KERNEL_OBJ}" TARGET_ARCH="${TARGET_ARCH_KERNEL}" -j"${KERNEL_BUILD_JOBS}" "${CROSS_COMPILE_MAKE}" "${IMAGE_TARGET}" modules
-make -C "${KERNEL_OBJ}" TARGET_ARCH="${TARGET_ARCH_KERNEL}" -j"${KERNEL_BUILD_JOBS}" "${CROSS_COMPILE_MAKE}"  modules
+make SYSSRC="${KERNEL_SRC}" SYSOUT="${KERNEL_OBJ}" TARGET_ARCH="${TARGET_ARCH_KERNEL}" -j"${KERNEL_BUILD_JOBS}" "${CROSS_COMPILE_MAKE}"  modules
 
-make -C "${KERNEL_OBJ}" TARGET_ARCH="${TARGET_ARCH_KERNEL}" -j"${KERNEL_BUILD_JOBS}" "${CROSS_COMPILE_MAKE}" INSTALL_MOD_PATH="${MODULES_INSTALL_PATH}" modules_install
-# REAL_SCRIPT="$(realpath "${0}")"
-# cd "$(dirname "${REAL_SCRIPT}")/../.."
-# KERNEL_DIR="$(realpath "${PWD}")"
+make SYSSRC="${KERNEL_SRC}" SYSOUT="${KERNEL_OBJ}" TARGET_ARCH="${TARGET_ARCH_KERNEL}" -j"${KERNEL_BUILD_JOBS}" "${CROSS_COMPILE_MAKE}" INSTALL_MOD_PATH="${MODULES_INSTALL_PATH}" modules_install
 
-# cd "${KERNEL_DIR}"
+cd $OLDPWD
