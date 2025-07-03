b23ed6e23dd812c2fa4832a6eccfd8267236afce
Benjamin Leggett <benjamin@edera.io>
2025-07-02 20:11:48 -0400

    Fix

--- hack/build/nvidiagpu-common.sh
+++ hack/build/nvidiagpu-common.sh
@@ -8,13 +8,15 @@ fi
 NV_KMOD_REPO_OWNER=NVIDIA
 NV_KMOD_REPO_NAME=open-gpu-kernel-modules
 NV_EXTRACT_PATH=/tmp/nvidia-modules
-NV_DOWNLOAD_PATH=/tmp/nvidia-modules.zip
+NV_DOWNLOAD_PATH=/tmp/nvidia-modules
+
+rm -rf "$NV_EXTRACT_PATH"
 
 echo "Fetching latest nvidia module release"
 
 LATEST_RELEASE_URL=$(curl -s "https://api.github.com/repos/${NV_KMOD_REPO_OWNER}/${NV_KMOD_REPO_NAME}/releases/latest" | \
-    grep -o '"zipball_url": *"[^"]*"' | \
-    sed 's/"zipball_url": *"\(.*\)"/\1/')
+    grep -o '"tarball_url": *"[^"]*"' | \
+    sed 's/"tarball_url": *"\(.*\)"/\1/')
 
 if [ -z "$LATEST_RELEASE_URL" ]; then
     echo "Failed to fetch latest release URL"
@@ -22,17 +24,20 @@ if [ -z "$LATEST_RELEASE_URL" ]; then
 fi
 
 echo "Downloading from: $LATEST_RELEASE_URL"
-
-curl -L -o "$NV_DOWNLOAD_PATH" "$LATEST_RELEASE_URL"
+curl -L -o "$NV_DOWNLOAD_PATH.tar.gz" "$LATEST_RELEASE_URL"
 mkdir -p "$NV_EXTRACT_PATH"
-unzip -q "$NV_DOWNLOAD_PATH" -d "$NV_EXTRACT_PATH"
+tar -xzf "$NV_DOWNLOAD_PATH.tar.gz" -C "$NV_EXTRACT_PATH"
 
 OLDPWD=$(pwd)
 cd "$NV_EXTRACT_PATH"/"$NV_KMOD_REPO_OWNER"-*
 
-# make SYSSRC=~/Source/edera-dev/linux-kernel-oci/linux-6.14.6 SYSOUT=/home/bleggett/Source/edera-dev/linux-kernel-oci/obj INSTALL_MOD_PATH=/home/bleggett/Source/edera-dev/linux-kernel-oci/obj modules_instal
-make SYSSRC="${KERNEL_SRC}" SYSOUT="${KERNEL_OUT}" TARGET_ARCH="${TARGET_ARCH_KERNEL}" -j"${KERNEL_BUILD_JOBS}" "${CROSS_COMPILE_MAKE}"  modules
+make -C . NV_VERBOSE=1 SYSSRC="${KERNEL_SRC}" SYSOUT="${KERNEL_OBJ}" TARGET_ARCH="${TARGET_ARCH_KERNEL}" -j"${KERNEL_BUILD_JOBS}" "${CROSS_COMPILE_MAKE}"  modules
 
-make SYSSRC="${KERNEL_SRC}" SYSOUT="${KERNEL_OUT}" TARGET_ARCH="${TARGET_ARCH_KERNEL}" -j"${KERNEL_BUILD_JOBS}" "${CROSS_COMPILE_MAKE}" INSTALL_MOD_PATH="${MODULES_INSTALL_PATH}" modules_install
+echo "Nvidia build done"
+
+make -C . NV_VERBOSE=1 SYSSRC="${KERNEL_SRC}" SYSOUT="${KERNEL_OBJ}" TARGET_ARCH="${TARGET_ARCH_KERNEL}" -j"${KERNEL_BUILD_JOBS}" "${CROSS_COMPILE_MAKE}" INSTALL_MOD_PATH="${MODULES_INSTALL_PATH}" modules_install
+
+echo "Nvidia install done"
 
 cd "$OLDPWD"
+
