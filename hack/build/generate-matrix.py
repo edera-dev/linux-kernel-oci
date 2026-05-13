080bbdc99c65775880c51fe90e2c085cdd53cc38
Simon Veith <simon@edera.dev>
2026-05-13 11:13:15 +0200

    build: Move top-level logic under a main() function

--- hack/build/generate-matrix.py
+++ hack/build/generate-matrix.py
@@ -34,90 +34,98 @@ def construct_manual_matrix(exact_versions):
 
 DEFAULT_BUILD_SPEC = "new"
 
-if len(sys.argv) > 1:
-    build_spec = sys.argv[1]
-else:
-    build_spec = DEFAULT_BUILD_SPEC
 
-if len(build_spec) == 0:
-    build_spec = DEFAULT_BUILD_SPEC
+def main():
+    if len(sys.argv) > 1:
+        build_spec = sys.argv[1]
+    else:
+        build_spec = DEFAULT_BUILD_SPEC
 
-build_spec_type = build_spec.split(":", maxsplit=1)[0]
-if ":" in build_spec:
-    build_spec_data = build_spec.split(":", maxsplit=1)[1]
-else:
-    build_spec_data = ""
+    if len(build_spec) == 0:
+        build_spec = DEFAULT_BUILD_SPEC
 
-constraint = {}
-if len(build_spec_data) > 0:
-    constraint = parse_text_constraint(build_spec_data)
+    build_spec_type = build_spec.split(":", maxsplit=1)[0]
+    if ":" in build_spec:
+        build_spec_data = build_spec.split(":", maxsplit=1)[1]
+    else:
+        build_spec_data = ""
 
-apply_config_versions = True
+    constraint = {}
+    if len(build_spec_data) > 0:
+        constraint = parse_text_constraint(build_spec_data)
 
-if build_spec_type == "new":
-    first_matrix = matrix.filter_new_builds(construct_all_matrix())
-elif build_spec_type == "rebuild":
-    first_matrix = construct_all_matrix()
-elif build_spec_type == "unsafe-all":
-    first_matrix = construct_all_matrix()
-    apply_config_versions = False
-elif build_spec_type == "stable":
-    first_matrix = construct_stable_matrix()
-elif build_spec_type == "lts":
-    first_matrix = construct_lts_matrix()
-elif build_spec_type == "only-latest-lts":
-    first_matrix = construct_lts_matrix()
-    apply_config_versions = False
-    matrix.sort_matrix(first_matrix)
-    last_version = parse(first_matrix[-1]["version"]).base_version
-    last_version_builds = list(
-        filter(lambda build: last_version in build["version"], first_matrix)
-    )
-    first_matrix = last_version_builds
-elif build_spec_type == "only-latest":
-    first_matrix = construct_stable_matrix()
-    apply_config_versions = False
-    matrix.sort_matrix(first_matrix)
-    last_version = parse(first_matrix[-1]["version"]).base_version
-    last_version_builds = list(
-        filter(lambda build: last_version in build["version"], first_matrix)
-    )
-    first_matrix = last_version_builds
-elif build_spec_type == "override":
-    first_matrix = construct_all_matrix()
-    apply_config_versions = False
-elif build_spec_type == "manual":
-    versions = maybe(constraint, "exact")
-    if versions is None:
-        versions = []
-    first_matrix = construct_manual_matrix(versions)
-    apply_config_versions = False
-else:
-    raise Exception("unknown build spec type: %s" % build_spec_type)
+    apply_config_versions = True
 
-if apply_config_versions:
-    final_matrix = matrix.filter_config_versions(first_matrix)
-else:
-    final_matrix = first_matrix
+    if build_spec_type == "new":
+        first_matrix = matrix.filter_new_builds(construct_all_matrix())
+    elif build_spec_type == "rebuild":
+        first_matrix = construct_all_matrix()
+    elif build_spec_type == "unsafe-all":
+        first_matrix = construct_all_matrix()
+        apply_config_versions = False
+    elif build_spec_type == "stable":
+        first_matrix = construct_stable_matrix()
+    elif build_spec_type == "lts":
+        first_matrix = construct_lts_matrix()
+    elif build_spec_type == "only-latest-lts":
+        first_matrix = construct_lts_matrix()
+        apply_config_versions = False
+        matrix.sort_matrix(first_matrix)
+        last_version = parse(first_matrix[-1]["version"]).base_version
+        last_version_builds = list(
+            filter(lambda build: last_version in build["version"], first_matrix)
+        )
+        first_matrix = last_version_builds
+    elif build_spec_type == "only-latest":
+        first_matrix = construct_stable_matrix()
+        apply_config_versions = False
+        matrix.sort_matrix(first_matrix)
+        last_version = parse(first_matrix[-1]["version"]).base_version
+        last_version_builds = list(
+            filter(lambda build: last_version in build["version"], first_matrix)
+        )
+        first_matrix = last_version_builds
+    elif build_spec_type == "override":
+        first_matrix = construct_all_matrix()
+        apply_config_versions = False
+    elif build_spec_type == "manual":
+        versions = maybe(constraint, "exact")
+        if versions is None:
+            versions = []
+        first_matrix = construct_manual_matrix(versions)
+        apply_config_versions = False
+    else:
+        raise Exception("unknown build spec type: %s" % build_spec_type)
 
-if len(build_spec_data) > 0:
-    constraint = parse_text_constraint(build_spec_data)
-    final_matrix = matrix.filter_matrix(final_matrix, constraint)
+    if apply_config_versions:
+        final_matrix = matrix.filter_config_versions(first_matrix)
+    else:
+        final_matrix = first_matrix
 
-matrix.validate_produce_conflicts(final_matrix)
-matrix.fill_runners(final_matrix)
+    if len(build_spec_data) > 0:
+        constraint = parse_text_constraint(build_spec_data)
+        final_matrix = matrix.filter_matrix(final_matrix, constraint)
 
-branch_suffix = get_branch_tag_suffix()
-if branch_suffix:
-    for build in final_matrix:
-        build["tags"] = ["%s-%s" % (t, branch_suffix) for t in build["tags"]]
-        build["produces"] = ["%s-%s" % (p, branch_suffix) for p in build["produces"]]
+    matrix.validate_produce_conflicts(final_matrix)
+    matrix.fill_runners(final_matrix)
 
-merges = matrix.generate_merges(final_matrix)
+    branch_suffix = get_branch_tag_suffix()
+    if branch_suffix:
+        for build in final_matrix:
+            build["tags"] = ["%s-%s" % (t, branch_suffix) for t in build["tags"]]
+            build["produces"] = [
+                "%s-%s" % (p, branch_suffix) for p in build["produces"]
+            ]
 
-print("generated %s builds, %s merges" % (len(final_matrix), len(merges)))
-matrix.summarize_matrix(final_matrix)
+    merges = matrix.generate_merges(final_matrix)
 
-with open("matrix.json", "w") as mf:
-    json.dump({"builds": final_matrix, "merges": merges}, mf)
-    mf.write("\n")
+    print("generated %s builds, %s merges" % (len(final_matrix), len(merges)))
+    matrix.summarize_matrix(final_matrix)
+
+    with open("matrix.json", "w") as mf:
+        json.dump({"builds": final_matrix, "merges": merges}, mf)
+        mf.write("\n")
+
+
+if __name__ == "__main__":
+    main()
--- hack/build/patchlist.py
+++ hack/build/patchlist.py
@@ -4,45 +4,49 @@ from matrix import CONFIG
 from packaging.version import parse
 from util import matches_constraints, maybe
 
-if len(sys.argv) != 3:
-    print("Usage: patchlist <KERNEL_VERSION> <KERNEL_FLAVOR>")
-    exit(1)
 
-try:
-    target_version = parse(sys.argv[1])
-except Exception:
-    target_version = parse(sys.argv[1].split("-")[0])
-kernel_flavor = sys.argv[2]
-series = "%s.%s" % (target_version.major, target_version.minor)
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: patchlist <KERNEL_VERSION> <KERNEL_FLAVOR>")
+        exit(1)
+
+    try:
+        target_version = parse(sys.argv[1])
+    except Exception:
+        target_version = parse(sys.argv[1].split("-")[0])
+    kernel_flavor = sys.argv[2]
+    series = "%s.%s" % (target_version.major, target_version.minor)
+
+    patches = CONFIG["patches"]
+
+    apply_patches = []
+
+    for patch in patches:
+        if "patch" in patch:
+            file_names = [patch["patch"]]
+        else:
+            file_names = patch["patches"]
+        order = maybe(patch, "order")
+
+        if order is None:
+            order = 1
+
+        apply = matches_constraints(target_version, kernel_flavor, patch)
+
+        if apply:
+            for file_name in file_names:
+                apply_patches.append(
+                    {
+                        "patch": file_name,
+                        "order": order,
+                    }
+                )
+
+    apply_patches.sort(key=lambda p: p["order"])
+
+    for patch in apply_patches:
+        print("patches/%s" % patch["patch"])
 
 
-patches = CONFIG["patches"]
-
-apply_patches = []
-
-
-for patch in patches:
-    if "patch" in patch:
-        file_names = [patch["patch"]]
-    else:
-        file_names = patch["patches"]
-    order = maybe(patch, "order")
-
-    if order is None:
-        order = 1
-
-    apply = matches_constraints(target_version, kernel_flavor, patch)
-
-    if apply:
-        for file_name in file_names:
-            apply_patches.append(
-                {
-                    "patch": file_name,
-                    "order": order,
-                }
-            )
-
-apply_patches.sort(key=lambda p: p["order"])
-
-for patch in apply_patches:
-    print("patches/%s" % patch["patch"])
+if __name__ == "__main__":
+    main()
